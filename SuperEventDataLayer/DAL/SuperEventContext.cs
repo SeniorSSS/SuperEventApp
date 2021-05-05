084268078e178547ba3bdabbf50b1650a6467bb4
Jānis Strazdiņš <janis.strazdins@intrum.com>
2021-05-05 21:47:08 +0300

    Add project files.

---
+++ SuperEventDataLayer/DAL/SuperEventContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+using SuperEventDataLayer.Models;
+
+namespace SuperEventDataLayer.DAL
+{
+    public class SuperEventContext : DbContext
+    {
+        public SuperEventContext() : base("name=SuperEventConnectionString")
+        {
+
+        }
+
+        public DbSet<ParticipantData> Participants { get; set; }
+
+    }
+}
---
+++ SuperEventModels/Participant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SuperEventModels
+{
+    public class Participant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+    }
+}
